631d66a34cabf9acb32b4200d01aba61d78657b4
Soraya Valle <svalleortega@gmail.com>
2020-06-07 22:53:56 +0200

    Create keyframes in JS style to control start/stop of app

--- js/main.js
+++ js/main.js
@@ -2,14 +2,19 @@
 
 const container = document.querySelector('.container');
 const text = document.querySelector('#text');
+const pointer = document.querySelector('.pointer-container');
 
 const totalTime = 7500;
 const breatheTime = (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
+let interval;
+const pointerKeyFrames = new KeyframeEffect(pointer, [{transform: 'rotate(0deg)'}, {transform: 'rotate(360deg)'}], {duration: totalTime, iterations: Infinity});
+const pointerAnimation = new Animation(pointerKeyFrames, document.timeline);
 
-breatheAnimation();
+// breatheAnimation();
 
 function breatheAnimation() {
+  pointerAnimation.play();
   text.innerHTML = 'Inspira';
   container.className = 'container grow';
 
@@ -23,4 +28,21 @@ function breatheAnimation() {
   }, breatheTime);
 }
 
-setInterval(breatheAnimation, totalTime);
+function startAnimation() {
+  if (text.innerHTML === 'Empezar') {
+  breatheAnimation();
+  interval = setInterval(breatheAnimation, totalTime);
+  // text.addEventListener('mouseover', () => { text.innerHTML= 'Parar'});
+  } else {
+    pointerAnimation.cancel();
+    
+    clearInterval(interval);
+    clearTimeout(breatheTime);
+    clearTimeout(holdTime);
+    clearTimeout(totalTime);
+    text.innerHTML="Empezar";
+  //  document.addEventListener('transitionend', () => {text.innerHTML="Empezar"});
+  }
+}
+
+text.addEventListener('click', startAnimation);
