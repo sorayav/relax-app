26ff3d9d163f154e3aed28a7619add7a12783a68
Soraya Valle <svalleortega@gmail.com>
2020-06-07 19:35:13 +0200

    Add breath animations function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const container = document.querySelector('.container');
+const text = document.querySelector('#text');
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+breatheAnimation();
+
+function breatheAnimation() {
+  text.innerHTML = 'Inspira';
+  container.className = 'container grow';
+
+  setTimeout(() => {
+    text.innerHTML = 'Aguanta';
+
+    setTimeout(() => {
+      text.innerHTML = 'Espira';
+      container.className = 'container shrink';
+    }, holdTime);
+  }, breatheTime);
+}
+
+setInterval(breatheAnimation, totalTime);
